27e42eeee81702c37b618ec3877553ed2ba55707
shanz01 <shanz01@jumei.com>
2018-05-16 17:04:34 +0800

    i am dev1

--- app/src/main/java/com/gitdemo/cn/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/cn/gitdemo/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("bro", "dev 推到 master");
+        Log.d("bro", "i am dev1");
     }
 }
