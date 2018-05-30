e1dc3185d568a662868e4acc555c8474d25e4f4f
张善 <zhsh2980@163.com>
2018-05-30 15:12:01 +0800

    master add

--- app/src/main/java/com/gitdemo/cn/gitdemo/MainActivity.java
+++ app/src/main/java/com/gitdemo/cn/gitdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("bro", "dev 推到 master");
         Log.d("bro", "i am dev1");
+        Log.d("bro", "master + 1");
     }
 }
